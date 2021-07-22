be3d38f71ce8df2b224290b2e0491dd424128f17
Dishup <dkshitapoojari19@gmail.com>
2021-07-22 13:50:11 +0530

    Added mutiple.java

--- sum.java
+++ sum.java
@@ -2,5 +2,5 @@ public class sum{
 	public static void main(string args[]{
 		int nnum1=10,num2=20,sum;
 		sum=num1+num2;
-		System.out.println("sun of numbers :"+sum");
+		System.out.println("sum :"+sum");
 }}
